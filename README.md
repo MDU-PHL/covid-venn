dcfd14c25743877df2a17468ed3e8b19341fe64f
Wytamma Wirth <wytamma.wirth@me.com>
2022-07-22 17:19:14 +1000

    :sparkles: v1.0

---
+++ README.md
@@ -0,0 +1,46 @@
+# Covid Venn
+
+Shiny app for creating venn diagrams of covid variant/lineage mutations.
+
+![](images/count.png)
+
+## Installation
+
+Clone the repo from github.
+```
+git clone https://github.com/MDU-PHL/covid-venn.git && cd covid-venn.git
+```
+
+Install dependencies.
+
+```
+install.packages('shiny', 'VennDiagram', 'RColorBrewer')
+```
+
+## Running
+
+Start the shiny server.
+
+```R
+shiny::runApp()
+```
+
+Shiny server can also be started from `bash`
+
+```bash
+Rscript -e "shiny::runApp()"
+```
+
+Navigate to the URL printed in the console.
+
+![](images/mutations.png)
+
+By default 3 lineages are selected. You can modify this selection using the *Lineages* select input. Up to 4 lineages can be compared. 
+
+There are two types of mutations that can be selected: VinTEBS (Variations in therapeutic epitopes or binding sites) and (VinOEBS= Variations in other epitopes or binding sites).
+
+Mutation labels can be shown using the *Show mutations* radio buttons. 
+
+## Data
+
+Covid-venn uses data from the [ncbi](https://www.ncbi.nlm.nih.gov/activ) and downloads the latest version of the database each time it runs. If the database is unavailable covid-venn will default to using a local copy of the database (which may be out of data).
---
+++ app.R
@@ -0,0 +1,178 @@
+library(shiny)
+library(VennDiagram)
+library(RColorBrewer)
+
+url <-
+    'https://www.ncbi.nlm.nih.gov/genomes/VirusVariation/vvsearch_dev/?report=download_lineages'
+# lineages <- read.csv('lineages.csv')
+lineages <- tryCatch({
+    lineages <- read.csv(url)
+},
+error = function(cond) {
+    print(cond)
+    message(paste("URL not working:", url))
+    message(paste("Loading local DB:", url))
+    lineages <- read.csv('lineages.csv')
+    return(lineages)
+})
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(# Application title
+    titlePanel("SARS-CoV-2 Variants Venn"),
+
+    # Sidebar
+    sidebarLayout(
+        sidebarPanel(
+            selectizeInput(
+                'selected_lineages',
+                'Lineages',
+                lineages$lineage,
+                options = list(maxItems = 4),
+                selected = c("BA.5", "BA.1", "BA.2.75"),
+                multiple = T
+            ),
+
+            checkboxGroupInput(
+                'selected_types',
+                'Types of mutations',
+                selected = c('VinTEBS', 'VinOEBS'),
+                choiceNames = c('VinTEBS (therapeutic)', 'VinOEBS (other)'),
+                choiceValues = c('VinTEBS', 'VinOEBS')
+            ),
+            radioButtons('show_mutations', 'Show mutations', c('yes', 'no'),
+                         'yes'),
+            HTML(
+                'Created using data from the <a href="https://www.ncbi.nlm.nih.gov/activ">ncbi SARS-CoV-2 Variants</a> Overview.'
+            ),
+        ),
+        # Show Venn
+        mainPanel(fluidRow(align = "center",
+                           plotOutput("venn")))
+    ))
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    get_mutations <- function(df, lineage, types) {
+        df[df$lineage == lineage, names(df) %in% append(types, 'lineages')]
+    }
+
+
+    output$venn <- renderPlot({
+        mutations <-
+            lapply(input$selected_lineages, function (x) {
+                paste0(get_mutations(lineages, x, input$selected_types),
+                       collapse = '')
+            })
+        number_of_groups <- length(input$selected_lineages)
+        if (input$show_mutations == 'yes') {
+            label_size <- 0.7
+            # force venn into predictable shape so we can edit the labels
+            if (number_of_groups == 3) {
+                groups <- list(c('a', 'c'), c('b', 'c'), c('c', 'd'))
+            } else if (number_of_groups == 2) {
+                groups <- list(c('a', 'c'), c('b', 'c'))
+            } else {
+                groups <- lapply(mutations, function(x) {
+                    unlist(strsplit(x, ","))
+                })
+                label_size <- 1.5
+            }
+
+        } else {
+            groups <- lapply(mutations, function(x) {
+                unlist(strsplit(x, ","))
+            })
+            label_size <- 1.5
+        }
+        if (length(mutations) == 0) {
+            return('No Mutations')
+        }
+        v <- venn.diagram(
+            groups,
+            category.names = input$selected_lineages,
+            filename = NULL,
+            height = 480 ,
+            width = 480 ,
+            resolution = 300,
+            euler.d = T,
+
+            # Circles
+            lwd = 2,
+            lty = 'blank',
+            fill = brewer.pal(4, "Pastel2")[0:length(input$selected_lineages)],
+
+            # Numbers
+            cex = label_size,
+            fontface = "bold",
+            fontfamily = "sans",
+
+            cat.cex = 1.5,
+            cat.fontfamily = "sans",
+
+            # Logging
+            disable.logging = T
+        )
+
+        split_to_lines <- function(x, n = 2) {
+            return (lapply(split(
+                x,
+                rep(1:(length(x) %/% n), length.out = length(x))
+            ),
+            function(x) {
+                paste(x, collapse = " ")
+            }))
+        }
+
+        make_lable <- function(set, labels_per_line = 3) {
+            paste(length(set),
+                  paste(
+                      split_to_lines(set, n = labels_per_line),
+                      collapse = "\n"
+                  ),
+                  sep = '\n')
+        }
+
+        if (input$show_mutations == 'yes') {
+            if (number_of_groups == 1) {
+                group1 <- unlist(strsplit(mutations[[1]], ","))
+                v[[3]]$label <- make_lable(group1)
+            }
+            if (number_of_groups == 2) {
+                group1 <- unlist(strsplit(mutations[[1]], ","))
+                group2 <- unlist(strsplit(mutations[[2]], ","))
+                g1 <- setdiff(group1, group2)
+                g2 <- setdiff(group2, group1)
+                g12 <- intersect(group1, group2)
+                v[[5]]$label <- make_lable(g1)
+                v[[6]]$label <- make_lable(g2)
+                v[[7]]$label <- make_lable(g12)
+
+            }
+            if (number_of_groups == 3) {
+                group1 <- unlist(strsplit(mutations[[1]], ","))
+                group2 <- unlist(strsplit(mutations[[2]], ","))
+                group3 <- unlist(strsplit(mutations[[3]], ","))
+                g1 <- setdiff(group1, union(group2, group3))
+                g2 <- setdiff(group2, union(group1, group3))
+                g3 <- setdiff(group3, union(group1, group2))
+                g12 <- setdiff(intersect(group1, group2), group3)
+                g13 <- setdiff(intersect(group1, group3), group2)
+                g23 <- setdiff(intersect(group2, group3), group1)
+                g123 <- intersect(intersect(group2, group1), group3)
+                v[[7]]$label <- make_lable(g1)
+                v[[8]]$label <- make_lable(g12)
+                v[[9]]$label <- make_lable(g2)
+                v[[10]]$label <- make_lable(g13)
+                v[[11]]$label <- make_lable(g123)
+                v[[12]]$label <- make_lable(g23)
+                v[[13]]$label <- make_lable(g3)
+            }
+
+        }
+        grid.newpage()
+        grid.draw(v)
+    }, height = 600, width = 600)
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
